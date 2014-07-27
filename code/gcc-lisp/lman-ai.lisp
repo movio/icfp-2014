b223945d2bbcdf899e6bc1e5dd1e3ae8b71c35db
Jonathan Chow <jonathan@movio.co>
2014-07-28 09:42:39 +1200

    Fixes paths dropping paths < PATH_LENGTH when it hits a wall.

--- code/gcc-lisp/lman-ai.lisp
+++ code/gcc-lisp/lman-ai.lisp
@@ -15,11 +15,14 @@
   ;; entry point for each move
 
   (defun step (ai-state Q)
-    (dbg (get-paths-from (lman-pos Q) (gen-is-not-wall-at Q))))
+    (let ((world          (get-world Q))
+          (lman-pos       (get-lman-pos Q))
+          (at-pos         (close-1-1 at-world world)))
+      (dbg (get-paths-from lman-pos (lambda (pos) (> (at-pos pos) 0))))))
 
   ;; AI !
 
-;; THIS FUNCTION IS BROKEN, WON'T COMPILE
+;; THIS FUNCTION IS BROKEN, WON'T COMPILE - probably mismatched parens
 ;;  (defun at-world-scored (Q)
 ;;    (lambda (pos)
 ;;      (let ((ghost-poss (map (ghost-states Q) ghost-pos))
@@ -31,33 +34,29 @@
 ;;            (score (nth (SCORES) val)))
 ;;        score))))
 
-  (defun gen-is-not-wall-at (Q)
-    (let ((this-world (world Q))
-          (gen-fun    (close-1-1 at-world this-world)))
-      (lambda (pos)
-        (> (gen-fun pos) 0))))
-
-  ;; helper functions
-
-  ; returns [path]
+  ;; returns all paths up to length PATH_LENGTH from the given position
   (defun get-paths-from (pos is-not-wall)
     (get-paths-iter (mklist (mklist pos)) is-not-wall (PATH_LENGTH)))
   (defun get-paths-iter (paths is-not-wall depth)
     (if (= 0 depth)
-      paths
-      (get-paths-iter
+      (map paths (lambda (path)           ;; if done
+                   (cdr (reverse path)))) ;; drop current position and correct order in list
+      (get-paths-iter                     ;; otherwise recursively extend each path
         (mapcat paths (lambda (path)
-                        (map
-                          (filter
-                            (find-urdl (car path))               ;; positions to consider
-                            (lambda (pos)
-                              (and
-                                (not (exists path
-                                       (lambda (old-pos)
-                                         (teq old-pos pos))))    ;; exclude spaces i've already been on
-                                (is-not-wall pos))))             ;; excluding walls
-                          (lambda (pos)
-                            (cons pos path)))))                  ;; prepended to the existing path
+                        (let ((good-urdl (filter
+                                          (find-urdl (car path))            ;; positions to consider
+                                          (lambda (pos)
+                                            (and
+                                              (not (exists path
+                                                     (lambda (old-pos)
+                                                       (teq old-pos pos)))) ;; exclude visited spaces
+                                              (is-not-wall pos))))))        ;; exclude walls
+                          (if (atom? good-urdl)       ;; if no more good positions
+                            (mklist path)             ;; return path so we don't lose it
+                            (map
+                              good-urdl
+                              (lambda (pos)
+                                (cons pos path))))))) ;; otherwise prepend each spot to the existing path
         is-not-wall
         (- depth 1))))
 
@@ -76,25 +75,25 @@
 
   ;; extractors from game-state
 
-  (defun fruit-state (Q)
+  (defun get-fruit-state (Q)
     (nth Q 3))
 
-  (defun ghost-pos (ghost-state)
+  (defun get-ghost-pos (ghost-state)
     (nth ghost-state 1))
 
-  (defun ghost-states (Q)
+  (defun get-ghost-states (Q)
     (nth Q 2))
 
-  (defun lman-pos (Q)
-    (nth (lman-state Q) 1))
+  (defun get-lman-pos (Q)
+    (nth (get-lman-state Q) 1))
 
-  (defun lman-dir (Q)
-    (nth (lman-state Q) 2))
+  (defun get-lman-dir (Q)
+    (nth (get-lman-state Q) 2))
 
-  (defun lman-state (Q)
+  (defun get-lman-state (Q)
     (nth Q 1))
 
-  (defun world (Q)
+  (defun get-world (Q)
     (nth Q 0))
 
   ;; OLD AI
@@ -106,15 +105,15 @@
           (zip-with-index
             (map
               (map
-                (find-urdl (lman-pos Q))
-                (close-1-1 at-world (world Q)))
+                (find-urdl (get-lman-pos Q))
+                (close-1-1 at-world (get-world Q)))
               (lambda (x)
                 (if (>= x 3) 1 x)))) ;; normalise world
           (lambda (v-dir)
-            (not (= (cdr v-dir) (nth (mklist 2 3 0 1) (lman-dir Q)))))) ;; lookup table for opposite dir
+            (not (= (cdr v-dir) (nth (mklist 2 3 0 1) (get-lman-dir Q)))))) ;; lookup table for opposite dir
         (cons 0 0)
         (lambda (max candidate)
-          (if (>= (car candidate) (car max))
+          (if (> (car candidate) (car max))
             candidate
             max)))))
 )
